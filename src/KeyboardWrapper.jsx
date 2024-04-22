2a2009c2ebe2e04472751d9153f9a8e7220c3365
lucasackerberg <lucas@ackerberg.se>
2024-04-22 13:40:25 +0200

    update
    update

---
+++ src/KeyboardWrapper.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import './KeyboardWrapper.css'
+
+function KeyboardWrapper() {
+  const letterArray = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','M','N','B','V','C','X','Z'];
+
+  return (
+    <div className="flex flex-wrap h-60 w-120 gap-3 text-center">
+      {letterArray.map((letter, index) => (
+        <button key={index} className="text-center justify-center flex h-20 w-20 text-2xl bg-black text-white">{letter}</button>
+      ))}
+    </div>
+  );
+}
+
+export default KeyboardWrapper;
