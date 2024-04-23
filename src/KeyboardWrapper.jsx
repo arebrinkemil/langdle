aa445431f0113cb3b8cd293f2d9f51d39d708b54
lucasackerberg <lucas@ackerberg.se>
2024-04-23 13:56:08 +0200

    keyboard working
    keyboard working

--- src/KeyboardWrapper.jsx
+++ src/KeyboardWrapper.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
 import './KeyboardWrapper.css'
 
-function KeyboardWrapper() {
+function KeyboardWrapper( {insertLetter} ) {
   const letterArray = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','M','N','B','V','C','X','Z'];
+
+  const inputToInputField = (letter) => {
+    insertLetter(letter);
+  }
 
   return (
     <div className="flex flex-wrap h-2/5 w-2/4 gap-3 text-center">
       {letterArray.map((letter, index) => (
-        <button 
+        <button
+          onClick={() => inputToInputField(letter)} 
           key={index}
           className="text-center items-center justify-center flex h-20 w-1/12 text-2xl bg-black text-white">{letter}</button>
       ))}
