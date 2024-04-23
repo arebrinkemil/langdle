1393d910833b54aef6f9fb7dda145982fb88c54c
lucasackerberg <lucas@ackerberg.se>
2024-04-23 13:31:53 +0200

    keyboard working
    keyboard working

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,12 @@ const App = () => {
   const [colorArray, setColorArray] = useState([]);
   const [correct, setCorrect] = useState(0);
 
+  const insertLetter = (letter) => {
+    if( input.length < 5 ) {
+      setInput(input + letter);
+    }
+  }
+
   const fetchInfo = async () => {
     const promptContent =
       language === "Swedish"
@@ -198,7 +204,7 @@ const App = () => {
             </div>
           ))}
         </div>
-        <KeyboardWrapper></KeyboardWrapper>
+        <KeyboardWrapper insertLetter={insertLetter}></KeyboardWrapper>
       </div>
     </div>
   );
